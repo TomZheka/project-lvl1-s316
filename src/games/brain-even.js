58a84844afca39776f9573ba1bf883b7ae0f7e76
TomJK <zh.tomskih@yandex.ru>
2018-08-05 16:23:54 +0300

    stroka

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,7 +6,6 @@ export const randomNumber = () => Math.floor(Math.random() * 100);
 
 const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
 
-
 const gameEven = () => {
   game('Answer "yes" if number even otherwise answer "no".', randomNumber, isEven);
 };
